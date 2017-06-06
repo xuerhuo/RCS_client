26d38446178d7b48d17000f2317dcb4bb46c72c0
xuerhuo <824851582@qq.com>
2017-06-06 09:19:13 +0800

    Initial commit

---
+++ RCS_client.h
@@ -0,0 +1,11 @@
+//
+// Created by erhuo on 2017/6/6.
+//
+
+#ifndef RCS_CLIENT_RCS_CLIENT_H
+#define RCS_CLIENT_RCS_CLIENT_H
+
+#include <string>
+
+const std::string CLIENT_PATH="C:\\Windows";
+#endif //RCS_CLIENT_RCS_CLIENT_H
